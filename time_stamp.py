730854e53668194ef77ddeb7ca6b03cfb31c0810
isaacmg <igodfried@isaac26.com>
2023-12-18 13:50:08 -0500

    Revert "fixing the stuff r"
    This reverts commit 2af43f118b1ef3a82b6f4235910ccc5a23f3fed8.

--- time_stamp.py
+++ time_stamp.py
@@ -1,11 +1,5 @@
 from transformers import OCRBertTokenizer, OCRBertForTokenClassification
-import cv2
 
 
-def load_model():
-    model = OCRBertForTokenClassification.from_pretrained("microsoft/ocr-bert-base")
-    tokenizer = OCRBertTokenizer.from_pretrained("microsoft/ocr-bert-base")
-    return model, tokenizer
-
-def get_frames(mp4_path):
+def extract_timestep():
     pass
