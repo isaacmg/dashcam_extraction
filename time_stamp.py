2af43f118b1ef3a82b6f4235910ccc5a23f3fed8
isaacmg <igodfried@isaac26.com>
2023-12-18 13:49:50 -0500

    fixing the stuff r

--- time_stamp.py
+++ time_stamp.py
@@ -1,5 +1,11 @@
 from transformers import OCRBertTokenizer, OCRBertForTokenClassification
+import cv2
 
 
-def extract_timestep():
+def load_model():
+    model = OCRBertForTokenClassification.from_pretrained("microsoft/ocr-bert-base")
+    tokenizer = OCRBertTokenizer.from_pretrained("microsoft/ocr-bert-base")
+    return model, tokenizer
+
+def get_frames(mp4_path):
     pass
